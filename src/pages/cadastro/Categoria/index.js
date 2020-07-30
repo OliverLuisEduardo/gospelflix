98170ce659fc87a1de464ac0d99931c6358b3199
OliverLuisEduardo <oliverluiseduardo@gmail.com>
2020-07-29 21:47:56 -0300

    Melhorado o index da adição de categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,42 +3,110 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-  const [categoria, setCategoria] = useState[('Teste')];
-  const [nomeCategoria, setNomeCategoria] = useState('Pregações');
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor:  '#000',
+  }
+
+  const [categoria, setCategoria] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
   
+  function setValue(chave, valor){
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
   return (
+
     <PageDefault>
-      <h1>Cadastro de Categoria: {nomeCategoria}</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
       <form onSubmit={function handleSubmit(infoEvento) {
         infoEvento.preventDefault();
-        console.log('Tentou form');
         setCategoria([
           ...categoria,
-          nomeCategoria
+          values
         ]);
+        setValues(valoresIniciais)
       }}>
+       
+       
+       <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+       {/* 
+      <div>
         <label>
           Nome da Categoria:
           <input
-            type="text"
-            value={nomeCategoria}
-            onChange={function Digitar(infoEvento){
-              setNomeCategoria (infoEvento.target.value);
+          type="text" 
+          value={values.nome}
+          onChange={function Digitar(infoEvento){
+          setValues ('nome', infoEvento.target.value);
             }}
           />
         </label>
+      </div>
+      
+      <div>
+        <label>
+          Descrição:
+          <textarea
+          type="text"
+          value={values.descricao}
+          onChange={function Digitar(infoEvento){
+            setValues ('descricao', infoEvento.target.value);
+            }}
+          />
+        </label>
+      </div>
 
-        <button>
-          Cadastrar
-        </button>
+      <div>
+        <label>
+          Cor:
+          <input
+          type="color"
+          value={values.cor}
+          onChange={function Digitar(infoEvento){
+          setValues (infoEvento.target.value);
+            }}
+          />
+        </label>
+      </div>
+          */}
+      <button>
+        Cadastrar
+      </button>
       </form>
 
       <ul>
-        {categoria.map((categoria) => {
+        {categoria.map((categoria, indice) => {
           return(
-            <li key={categoria}>
-              {categoria}
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
             </li>
           )
         })}
